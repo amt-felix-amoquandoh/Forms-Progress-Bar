48b52458a4f6ec98b43dae7e1dd0dcd2ff8a9d91
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-09-15 12:54:52 +0000

    initial commit

---
+++ bar.js
@@ -0,0 +1,5 @@
+const firstForm = document.getElementById("form1");
+const secondFrom = document.getElementById("form2");
+const thirdForm = document.getElementById("form3");
+const fourthForm = document.getElementById("form4");
+
