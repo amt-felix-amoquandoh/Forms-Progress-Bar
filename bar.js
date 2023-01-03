60d5e04f418232fd22373b4858d213900b6b835a
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2023-01-03 15:35:50 +0000

    Update bar.js

--- bar.js
+++
@@ -1,5 +0,0 @@
-const firstForm = document.getElementById("form1");
-const secondFrom = document.getElementById("form2");
-const thirdForm = document.getElementById("form3");
-const fourthForm = document.getElementById("form4");
-
