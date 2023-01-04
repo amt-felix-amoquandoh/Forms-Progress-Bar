f92e350fa85070a976bd4e94778da922e0de6d57
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2023-01-04 21:41:43 +0000

    Update bar.js

--- bar.js
+++ bar.js
@@ -4,3 +4,31 @@ const next  = document.getElementById("nextButton");
 const circles = document.querySelectorAll(".circles");
 
 let currentActive = 1;
+
+next.addEventListener("click", () => {
+    currentActive++;
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length        
+    }
+    update();
+});
+
+previous.addEventListener("click", () => {
+    currentActive--;
+
+    if (currentActive > 1) {
+        currentActive = 1        
+    }
+    update();
+});
+
+function update(){
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add("active");           
+        } else {
+            circle.classList.remove("active");
+        }
+    });
+}
