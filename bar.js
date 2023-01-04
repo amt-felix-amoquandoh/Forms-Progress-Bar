4402dae14a1966a87995359c724ca050de5f0047
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2023-01-04 21:33:25 +0000

    Update bar.js

--- bar.js
+++ bar.js
@@ -1,3 +1,6 @@
 const progress  = document.getElementById("progress");
 const previous  = document.getElementById("prevButton");
 const next  = document.getElementById("nextButton");
+const circles = document.querySelectorAll(".circles");
+
+let currentActive = 1
